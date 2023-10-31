1af6d3076c47f5d251e25e7f4ec181ee1dc1014e
Muktorov <muxtorovinomjon0227@gmail.com>
2023-10-31 18:06:43 +0500

    Call cannection to app

---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,16 @@
+import 'package:connectycube_sdk/connectycube_sdk.dart';
+
+const String APP_ID = "7236";
+const String AUTH_KEY = "JFPtjOKO5Y979D7";
+const String AUTH_SECRET = "zvsfjsUTngTwbqB";
+const String DEFAULT_PASS = "Flutter2000";
+
+List<CubeUser> users = [
+  CubeUser(
+    id: 11253684,
+    login: "Flutter2000",
+    fullName: "Inomjon",
+    password: DEFAULT_PASS,
+  ),
+
+];
